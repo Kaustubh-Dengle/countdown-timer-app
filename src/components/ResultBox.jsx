311f19403b3d2d7b1f15cc20dfdf46c680acfd38
Kaustubh-Dengle <dengle.kaustubh@gmail.com>
2024-06-11 23:27:55 +0530

    minor UI changes

--- src/components/ResultBox.jsx
+++ src/components/ResultBox.jsx
@@ -24,11 +24,11 @@ const ResultBox = forwardRef(function ResultBox(
       {userLost && <h2>You Lost!</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
       <p>
-        The target time was <strong>{targetTime} seconds.</strong>;
+        The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
         You stopped the timer with{" "}
-        <strong>{formattedRemainingTime} seconds left</strong>
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
